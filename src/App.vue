82f630b54032f78bb4ee6b81489690d37121b0bd
vanmi46 <vanmi46@gmail.com>
2025-10-29 16:41:39 +0100

    modif Battlelog

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import AppHeader from './components/AppHeader.vue'
 import Player from './components/Player.vue'
 import Attack from './components/Attack.vue'
+import Battlelog from './components/Battlelog.vue'
 </script>
 
 <template>
   <AppHeader />
   <main class="container">
   <router-view />
+  <Battlelog></Battlelog>
   </main>
 </template>
 
