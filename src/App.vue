38427f92f9852db363e46e0db7f9039a2f4ad6bc
NemesisRoss <160053298+NemesisRoss@users.noreply.github.com>
2025-10-29 16:00:10 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue'
-<<<<<<< HEAD:src/App.vue
 import Player from './components/Player.vue'
-=======
 import Attack from './components/Attack.vue'
->>>>>>> 9b187294219566903be5ebf73936d4b4c633bcde:jeu-pokemon/src/App.vue
 </script>
 
 <template>
